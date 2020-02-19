0625d3875a50e3e43585405138c7a8352bba0679
Dwayne-capacity <60339764+Dwayne-capacity@users.noreply.github.com>
2020-02-19 11:38:00 +0200

    Add files via upload

---
+++ food.sql
@@ -0,0 +1,11 @@
+Food_Item,Category,Size,Woolworths,Stock_Quantity_W,Checkers,Stock_Quantity_C,Pick_n_Pay,Stock_Quantity_P
+A001,Simba Beef Flavoured Potato Chips,Snacks,125g,18.49,13,15.99,21,15.49,5
+A002,Lays Salted Potato Chips,Snacks,125g,19.49,11,18.99,18,17.99,16
+A003,Coca-Cola Original Bottle,Beverages,2L,22.49,6,21.99,33,21.49,25
+A004,Fanta Orange Bottle,Beverages,2L,22.49,4,19.99,16,19.49,28
+A005,Cadbury Dairy Milk Chocolate Slab,Chocolates,80g,15.99,19,15.99,30,15.99,26
+A006,Nestlé Quality Street Chocolates,Chocolates,500g,99.99,6,99.99,12,99.99,5
+A007,Nescafé Gold Roasted Coffee,Coffee,200g,109.99,12,109.99,55,109.99,46
+A008,Jacobs Krönung Instant Coffee,Coffee,200g,109.99,15,104.00,21,109.99,58
+A009,All Gold Tomato Sauce,Sauces,700ml,30.99,2,26.99,26,25.99,9
+A010,Crosse & Blackwell Mayonnaise,Sauces,750g,33.99,4,31.99,22,29.99,29
